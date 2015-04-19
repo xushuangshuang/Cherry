c469a220a3fae54db5243aba959312bfb9166a8e
xushuangshuang <xushuangshuang1@gmail.com>
2015-04-19 08:45:59 +0800

    add weather picture and draw userPage

--- app/src/main/java/com/xushuangshuang/cherry/activity/WeatherActivity.java
+++ app/src/main/java/com/xushuangshuang/cherry/activity/WeatherActivity.java
@@ -2,7 +2,9 @@ package com.xushuangshuang.cherry.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.view.Window;
+import android.widget.ImageView;
 
 import com.xushuangshuang.cherry.R;
 
@@ -10,10 +12,20 @@ import com.xushuangshuang.cherry.R;
  * Created by xuss on 2015/4/19.
  */
 public class WeatherActivity extends Activity {
+
+    private ImageView backGroundImageView;
+    private ViewPager userViewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        initView();
+    }
+
+    private void initView(){
         setContentView(R.layout.weather_layout);
+        backGroundImageView = (ImageView) findViewById(R.id.imageView_user_bg);
+        userViewPager = (ViewPager) findViewById(R.id.viewPager_user);
     }
 }
